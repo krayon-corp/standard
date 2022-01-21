02268bc2267319ad01c6d9901b8873df773e15f0
Mark Ngishu <mark@krayonsoftware.com>
2022-01-21 08:49:55 -0600

    Add standard files

---
+++ standard/krayon_standard.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Krayon Corp. 2021
+// Hey so listen, if you don't work at Krayon and the framework department, you shouldn't be looking at this code UwU.
+
+#include <iostream>
+
+void* KrayonModuleMain() {
+	std::cout << "TITS XDXD" << std::endl;
+	// SO I HAVE LITERALLY NOTHING... UMM MAYBE LETS SEE IF THE KERNEL CAN LOAD IT IN...
+	return nullptr;
+}
